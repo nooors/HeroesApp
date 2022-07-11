92ddb8ccd56bcdda7959a643d79b457f5919678a
Jose Nortes <nooors@gmail.com>
2022-07-11 16:53:58 +0200

    layout improves

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -5,7 +5,22 @@ import { AuthService } from "../../services/auth.service";
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
-  styles: [],
+  styles: [
+    `
+      .container {
+        height: 90vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      mat-card-content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 2rem;
+      }
+    `,
+  ],
 })
 export class LoginComponent implements OnInit {
   constructor(private router: Router, private httpSrv: AuthService) {}
@@ -14,9 +29,11 @@ export class LoginComponent implements OnInit {
   login() {
     // Ïr al backend
     // un usuario
-    this.httpSrv.login().subscribe((response) => console.log(response));
-    // disponible en toda la aplicación
-    // si todo sale bien ir a la pagina de list heroes
-    this.router.navigate(["/heroes"]);
+    this.httpSrv.login().subscribe((response) => {
+      console.log(response);
+      // disponible en toda la aplicación
+      // si todo sale bien ir a la pagina de list heroes
+      this.router.navigate(["/heroes"]);
+    });
   }
 }
