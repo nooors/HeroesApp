01362b710d621a33d346cb3547167e96d84a7f17
Jose Nortes <nooors@gmail.com>
2022-08-23 09:47:03 +0200

    test: app-component tested

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { RouterOutlet } from "@angular/router";
 
 describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule.withRoutes([])],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -20,5 +22,12 @@ describe("AppComponent", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual("HeroesApp");
+  });
+
+  it("Should have an outlet-router directive", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    const elem = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(elem).not.toBeNull();
   });
 });
